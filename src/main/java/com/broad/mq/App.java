c8c60f652ad4342f8f8380c8ac02b06c940795ca
zhiyong.yue <zhiyong.yue@qiaofang.local>
2018-11-10 19:02:35 +0800

    init

---
+++ src/main/java/com/broad/mq/App.java
@@ -0,0 +1,13 @@
+package com.broad.mq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
